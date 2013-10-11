9bf301b165acdb21f5719cee4e63f57a9aed40a6
gytlak <gytis.lakavicius@stud.ktu.lt>
2013-10-11 13:58:42 +0300

    created settings.php

---
+++ settings.php
@@ -0,0 +1,10 @@
+<?php
+// user settings
+define('ADMIN_USERNAME', 'admin');
+define('ADMIN_PASSWORD', 'nfqakademija');
+
+// database settings
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_NAME', 'gallery');
